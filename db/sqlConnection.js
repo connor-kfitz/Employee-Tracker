0a9fadabe2211fa0c44f7905758cfb97d5fe630e
connor-kfitz <connorkfitzsimmons@gmail.com>
2022-06-23 13:13:21 -0400

    Imported Libraries and Connected SQL DB

---
+++ db/sqlConnection.js
@@ -0,0 +1,15 @@
+// Server Import
+const mysql = require('mysql2');
+
+// Connect to SQL
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      user: 'root',
+      password: 'password',
+      database: 'company_db'
+    },
+    console.log(`Connected to the company_db database.`)
+  );
+
+module.exports = db;
--- index.js
+++ index.js
@@ -1,45 +1,42 @@
 // Import
 const inquirer = require('inquirer');
-// const server = require('./server')
-
+const inquier = require('console.table');
 
 // Variables
 mainMenuPromptQuestions = ['View all departments','View all roles','View all employees','Add a department','Add a role','Add an employee','Update an employee role']
 
 // Functions
-function init() {
-inquirer
- .prompt([
-    {
-        type: 'list',
-        name: 'mainMenuPrompt',
-        message: 'What would you like to do?',
-        choices: mainMenuPromptQuestions
-    }
- ])
- .then((data) => {
-    if(data.mainMenuPromt == mainMenuPromptQuestions[0]){
-        // Function Placeholder
-    }
-    else if(data.mainMenuPromt == mainMenuPromptQuestions[1]){
-        // Function Placeholder
-    }
-    else if(data.mainMenuPromt == mainMenuPromptQuestions[2]){
-        // Function Placeholder
-    }
-    else if(data.mainMenuPromt == mainMenuPromptQuestions[3]){
-        // Function Placeholder
-    }
-    else if(data.mainMenuPromt == mainMenuPromptQuestions[4]){
-        // Function Placeholder
-    }
-    else if(data.mainMenuPromt == mainMenuPromptQuestions[5]){
-        // Function Placeholder
-    }
-    else if(data.mainMenuPromt == mainMenuPromptQuestions[6]){
-        // Function Placeholder
-    }
- });
-}
-
-// server.getDepartments();
+function mainMenu() {
+  inquirer
+   .prompt([
+      {
+          type: 'list',
+          name: 'mainMenuPrompt',
+          message: 'What would you like to do?',
+          choices: mainMenuPromptQuestions
+      }
+   ])
+   .then((data) => {
+      if(data.mainMenuPromt == mainMenuPromptQuestions[0]){
+          // Function Placeholder
+      }
+      else if(data.mainMenuPromt == mainMenuPromptQuestions[1]){
+          // Function Placeholder
+      }
+      else if(data.mainMenuPromt == mainMenuPromptQuestions[2]){
+          // Function Placeholder
+      }
+      else if(data.mainMenuPromt == mainMenuPromptQuestions[3]){
+          // Function Placeholder
+      }
+      else if(data.mainMenuPromt == mainMenuPromptQuestions[4]){
+          // Function Placeholder
+      }
+      else if(data.mainMenuPromt == mainMenuPromptQuestions[5]){
+          // Function Placeholder
+      }
+      else if(data.mainMenuPromt == mainMenuPromptQuestions[6]){
+          // Function Placeholder
+      }
+   });
+  }
